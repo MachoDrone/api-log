2e1cd5f4475e4a7477bd79eb0bda3ba019459328
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-05-10 09:32:25 -0400

    Update wsl-api-logs.sh

--- wsl-api-logs.sh
+++ wsl-api-logs.sh
@@ -7,5 +7,5 @@ echo "" >> api-logs.txt && echo "podman ps -a:" >> api-logs.txt && podman ps -a 
 echo "" >> api-logs.txt && echo "frpc-api log:" >> api-logs.txt && podman ps -a --format '{{.Names}}' | grep '^frpc-api-' | xargs -I {} podman logs -t {} >> api-logs.txt
 echo "" >> api-logs.txt && docker logs -t nosana-node | grep 'Wallet:' | awk '{print "Host Address: " $3}' >> api-logs.txt
 ls -ralsh api-logs.txt >> api-logs.txt
-if ! grep -qi Microsoft /proc/version && ! grep -qi WSL /proc/version; then echo -e "\033[31mTHIS SCRIPT IS MEANT FOR WINDOWS WSL2, NOT NATIVE UBUNTU LINUX\033[0m" | tee -a api-logs.txt; fi
+if ! grep -qi Microsoft /proc/version && ! grep -qi WSL /proc/version; then echo -e "\033[31mTHIS SCRIPT IS MEANT FOR WINDOWS WSL2, NOT NATIVE UBUNTU LINUX.. use the correct script for your OS\033[0m" | tee -a api-logs.txt; fi
 cat api-logs.txt

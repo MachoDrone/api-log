aeca265380b06154814b5ff45e4e31e72c47d99e
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-05-10 08:57:47 -0400

    Create api-logs.sh

---
+++ api-logs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "" > api-logs.txt && echo "docker log snippets:" >> api-logs.txt && docker logs --timestamps --since 24h nosana-node 2>/dev/null | grep -C 21 "API proxy is offline, restarting.." >> api-logs.txt && echo "" >> api-logs.txt
+echo "" >> api-logs.txt && echo "docker ps:" >> api-logs.txt && docker ps >> api-logs.txt
+echo "" >> api-logs.txt && echo "docker ps -a:" >> api-logs.txt && docker ps -a >> api-logs.txt
+echo "" >> api-logs.txt && echo "docker exec podman podman ps:" >> api-logs.txt && docker exec podman podman ps >> api-logs.txt
+echo "" >> api-logs.txt && echo "docker exec podman podman ps -a:" >> api-logs.txt && docker exec podman podman ps -a >> api-logs.txt
+echo "" >> api-logs.txt && echo "frpc-api log:" >> api-logs.txt && docker exec podman podman ps --format '{{.Names}}' | grep '^frpc-api-' | xargs -I {} docker exec podman podman logs -t {} >> api-logs.txt
+echo "" >> api-logs.txt && docker logs -t nosana-node | grep 'Wallet:' | awk '{print "Host Address: " $3}' >> api-logs.txt
+ls -ralsh api-logs.txt >> api-logs.txt
+cat api-logs.txt
